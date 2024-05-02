04bdf5ccfd44c148cd4d131a77e870a6eb37e766
Gabriel Santana <gabrielsantana77@hotmail.com>
2024-05-02 19:53:02 -0300

    Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Header } from './components/Header';
-import { Post } from './Post'
+import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
 
 import './global.css';
 
@@ -8,14 +11,19 @@ export function App() {
     <div>
       <Header />
 
-      <Post
-        author="Gabriel "
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
-      />
-      <Post
-        author="Gabriel "
-        content="Um novo post muito legal"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+          author="Gabriel "
+          content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+          />
+          <Post
+            author="Gabriel "
+            content="Um novo post muito legal"
+          />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/gabriel-santana.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Gabriel Santana</strong>
+              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom, parabéns</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
