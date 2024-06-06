929da5775025759631c70d78b6d1ea18ca9f6642
Gabriel Santana <gabrielsantana77@hotmail.com>
2024-06-06 19:59:50 -0300

    Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -11,7 +12,7 @@ export function Sidebar() {
       />
     
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/gabriel-santana.png" />
+        <Avatar src="https://github.com/gabriel-santana.png" />
 
         <strong>Gabriel Santana</strong>
         <span>Web Developer</span>
