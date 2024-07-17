4e17f6921416c541273bb61d9275b7d1eb687092
Gabriel Santana <gabriesantana@hotmail.com>
2024-07-16 23:24:19 -0300

    ignite-feed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Header } from './components/Header';
-import { Post } from './components/Post'
+import { Post, PostType } from './components/Post'
 import { Sidebar } from './components/Sidebar';
 
 import styles from './App.module.css';
 
 import './global.css';
 
-const posts = [
+
+
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -51,9 +53,7 @@ export function App() {
               return (
               <Post
                 key={post.id} 
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
+                post={post}
               />
             )
             })}
